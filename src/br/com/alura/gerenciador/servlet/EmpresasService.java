4aa5fb047ef61452eeeaa401572fa0bac99527ca
Giovane Bertol <giovanebertol@yahoo.com.br>
2022-07-07 23:10:26 -0300

    Alterações de Projeto

--- src/br/com/alura/gerenciador/servlet/EmpresasService.java
+++ src/br/com/alura/gerenciador/servlet/EmpresasService.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.List;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -26,10 +27,24 @@ public class EmpresasService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		List<Empresa> empresas = new Banco().getEmpresas();
 		
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);
-		response.setContentType("application/json");
-		response.getWriter().print(json);
+		String valor = request.getHeader("accept");
+		
+		if(valor.endsWith("xml")) {
+			XStream xstream = new XStream();			
+			String xml = xstream.toXML(empresas);
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+		}else if(valor.endsWith("json")) {
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+		}else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message':'no content'}");
+		}
+		
+		
 		
 		
 	}
